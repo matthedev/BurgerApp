1d0be501c53011112f7d84e4e2c8c7def3aad504
matthedev <mtuxiashvili@yahoo.com>
2020-10-20 19:27:46 -0700

    Updates

---
+++ precache-manifest.2641df650cddfcdbe51e9765e9418dc2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a288ab051faf6d92d9905eed4ae294e3",
+    "url": "/BurgerApp/index.html"
+  },
+  {
+    "revision": "397ece69b1410e47befe",
+    "url": "/BurgerApp/static/css/main.8b910b14.chunk.css"
+  },
+  {
+    "revision": "cbbc0cbed3f53959eea6",
+    "url": "/BurgerApp/static/js/2.8f070cea.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/BurgerApp/static/js/2.8f070cea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "397ece69b1410e47befe",
+    "url": "/BurgerApp/static/js/main.043077af.chunk.js"
+  },
+  {
+    "revision": "6c623bc1712ee81131a2",
+    "url": "/BurgerApp/static/js/runtime-main.13c4561a.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/BurgerApp/static/media/BurgerLogo.b8503d26.png"
+  }
+]);
